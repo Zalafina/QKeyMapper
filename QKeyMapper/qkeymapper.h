3406e988d42bf84e05f6921398b855f34c2584e7
Melvin Li <kkkwing15@163.com>
2025-01-27 03:00:53 +0800

    Fix Qt5 build error.
    Signed-off-by: Melvin Li <kkkwing15@163.com>

--- QKeyMapper/qkeymapper.h
+++ QKeyMapper/qkeymapper.h
@@ -42,13 +42,9 @@
 #include <windows.h>
 #ifndef max
 #define max(a,b) (((a) > (b)) ? (a) : (b))
-#else
-#error max macro is already defined
 #endif
 #ifndef min
 #define min(a,b) (((a) < (b)) ? (a) : (b))
-#else
-#error min macro is already defined
 #endif
 #include <Gdiplus.h>
 #include <process.h>
