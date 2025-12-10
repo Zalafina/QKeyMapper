2f373a7d2c3fe243ca657b76a31ac5474456e618
Melvin Li <kkkwing15@163.com>
2025-12-10 14:27:34 +0800

    Add FakerInput support.
    Signed-off-by: Melvin Li <kkkwing15@163.com>

--- QKeyMapper/qkeymapper_worker.h
+++ QKeyMapper/qkeymapper_worker.h
@@ -839,6 +839,7 @@ public:
     static int FakerInputClient_Connect(void);
     static void FakerInputClient_Disconnect(void);
     static void FakerInputClient_Free(void);
+    static void FakerInputClient_ReleaseAll(void);
     static FakerInputClient_ConnectState FakerInputClient_getConnectState(void);
     static void FakerInputClient_setConnectState(FakerInputClient_ConnectState connectstate);
     static bool FakerInputClient_sendKeyboardInput(quint8 vkeycode, bool extendedFlag, int keyupdown);
